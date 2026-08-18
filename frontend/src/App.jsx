80befdf2eb6b0db9a1698f0431190f01f1191b77
Zeyad Arafat <zeyadarafat833@gmail.com>
2026-08-18 03:16:12 +0300

    fixing sim

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import Header from "./components/Header";
 import MapView from "./components/MapView";
